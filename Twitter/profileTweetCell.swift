6b393736499070649286d0d4322a238d7d8b9d9f
Jeanne Nina <owner@MacBook-Pro.fios-router.home>
2016-02-27 04:07:57 -0500

    created all view controllers

---
+++ Twitter/profileTweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTweetCell.swift
+//  Twitter
+//
+//  Created by Jeanne Nina on 2/27/16.
+//  Copyright © 2016 Nina. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
